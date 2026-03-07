a8d79ab98a6ae569436f5c2b45afa2dedee4d161
nahidhasan2kk2 <nahidhasan2kk2@gmail.com>
2026-03-07 20:08:46 +0300

    added cards and design

--- js/home.js
+++ js/home.js
@@ -1,1 +1,16 @@
-console.log('home');
+let currentStatus = 'all';
+const activeTab = ['bg-primary', 'text-white'];
+const InActiveTab = ['bg-gray', 'text-black'];
+
+function switchTab(tab) {
+
+ const tabs = ["all", "open", "closed"];
+ for (const t of tabs) {
+  const tabName = document.getElementById(t + '-tab');
+  if (t === tab) {
+
+  }
+ }
+
+
+}
