cd429cfb2ef6cbcdb4ad73d4e75b138d5dbd2089
nahidhasan2kk2 <nahidhasan2kk2@gmail.com>
2026-03-08 21:00:54 +0300

    add model for all cards

--- js/home.js
+++ js/home.js
@@ -22,42 +22,28 @@ switchTab(currentStatus);
 
 const showArrayProducts = arr => arr.map(er => `<span class="bg-orange-300 text-red-700 px-2 rounded-lg">${er}</span>`).join(" ");
 
-const loadCardData = () => {
-  const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      displayCardData(data.data)
-      filtedData(data.data);
+const loadCardData = async () => {
 
-    })
+  const response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+  const data = await response.json();
+  displayCardData(data.data);
+  filterData(data.data);
+
 }
 
 const displayCardData = (datas) => {
-  // console.log(datas);
+
+
+
   const cardContainer = document.getElementById('cards-container');
   const countIssue = document.getElementById('count-issue');
   cardContainer.innerHTML = "";
-  // {
-  //       "id": 1,
-  //       "title": "Fix navigation menu on mobile devices",
-  //       "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-  //       "status": "open",
-  //       "labels": [
-  //         "bug",
-  //         "help wanted"
-  //       ],
-  //       "priority": "high",
-  //       "author": "john_doe",
-  //       "assignee": "jane_smith",
-  //       "createdAt": "2024-01-15T10:30:00Z",
-  //       "updatedAt": "2024-01-15T10:30:00Z"
-  //     },
   countIssue.innerText = datas.length;
   for (const data of datas) {
+
+
     let priorityColor = "";
     let borderColor = "";
-
 
     if (data.priority === "high") {
       priorityColor = "bg-red-300 text-red-700";
@@ -77,7 +63,7 @@ const displayCardData = (datas) => {
     }
     // console.log(data.status);
     cardContainer.innerHTML += `
-     <div class="card bg-base-100 ${borderColor}  shadow-lg p-5 space-y-5 ">
+     <div onclick="loadModalCardData(${data.id})" class="card bg-base-100 ${borderColor} shadow-lg p-5 space-y-5 ">
        <div class="flex justify-between items-center">
         <img src="./assets/${data.status}.status.png" alt="">
         <button class="bg-red-300 text-red-700 px-2 ${priorityColor} rounded-lg">${data.priority}</button>
@@ -101,26 +87,46 @@ ${showArrayProducts(data.labels)}
 
 }
 
-const filtedData = (datas) => {
+const filterData = (datas) => {
 
   document.getElementById('all-tab').addEventListener('click', function () {
     displayCardData(datas);
-    console.log(datas);
+
 
   })
   document.getElementById('open-tab').addEventListener('click', function () {
     const openFilterData = datas.filter(data => data.status === 'open');
-    console.log(openFilterData);
+
     displayCardData(openFilterData);
   })
   document.getElementById('closed-tab').addEventListener('click', function () {
     const closedFilterData = datas.filter(data => data.status === 'closed');
-    console.log(closedFilterData);
+
     displayCardData(closedFilterData);
   })
 }
 
 
+const loadModalCardData = async (id) => {
+  const response = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+  const datas = await response.json();
+  displayModalCard(datas.data);
 
+
+}
+
+const displayModalCard = (datas) => {
+  const modalBox = document.getElementById('modal-card-box');
+  modalBox.innerHTML = `
+  <h1>hello</h1>
+  <div class="modal-action">
+    <form method="dialog">
+     <!-- if there is a button in form, it will close the modal -->
+     <button class="btn">Close</button>
+    </form>
+   </div>
+  `
+  document.getElementById('model_card').showModal();
+}
 
 loadCardData();
